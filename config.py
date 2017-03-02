a29f11ca02334e85d3e5e9467fa8bceb87e7b4c9
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-03-02 15:42:29 -0800

    add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_URL = 'mongodb://productivityintech.com'
+PORT = 36161
+DATABASE = 'pitpodcast'
+FILE_STORAGE_LOCATION = '/mnt/volume-sfo2-01/files/podcasts/'
+WEBSITE_URL = 'http://productivityintech.com'
+OWNER = {'name': 'Jay Miller',
+        'email': 'kjaymiller@gmail.com'}
