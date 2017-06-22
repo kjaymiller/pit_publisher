62c5d26fe31cfb71bafaefbf8a3673858aed40d0
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-06-21 21:17:00 -0700

    Added edit_shownotes Thanks @chriswaterguy for the tweet to make me do this

---
+++ edit_shownotes.py
@@ -0,0 +1,35 @@
+"""
+Edit Podcast Shownotes
+created: 21 Jun 2017
+Author: Jay Miller @kjaymiller
+
+Description:
+The following file downloads the requested shownotes and replaces it with the modified version
+Special Thanks to @ChrisWaterguy for pointing out the issue that made me write this! Long time coming, thanks Chris for being the straw that broke the camel's back.
+
+example `python edit_shownotes.py some_podcast 42`
+"""
+
+from config import DEFAULT_UPLOAD_SOURCE
+from sys import argv
+from podcasts import podcasts
+
+podcast = podcasts[argv[0]]
+episode_number = argv[1]
+
+# Download the file to be edited
+episode = podcast.collection.find_one({'episode_number': episode_number})
+shownotes = episode['content']
+file_path = f'{DEFAULT_UPLOAD_SOURCE}/{shownotes}.md'
+with open(file_path, 'w') as f:
+    f.write(shownotes)
+
+# pause and wait for enter
+phrase = """Enter 'q' or 'quit' and press return[Enter] to exit.
+The file will not be removed from your computer: """
+if input.lower() in ('quit', 'q'):
+    with open(file_path, 'r') as f:
+        f = f.read()
+        podcast.collection.find_one_and_update(
+            {'episode_number': episode_number},
+            {'$set': {'content': f}})
